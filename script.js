2ea736550dfa3cd9dca1e9706ceb16286a0731f7
Ionel Boicu <119396656+ionel96@users.noreply.github.com>
2022-12-04 21:41:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let numOfButtons = 3;
+let winnerButton = Math.ceil(Math.random() * numOfButtons);
 function checkButton(clickedButton) {
-    let winnerButton = Math.ceil(Math.random() * numOfButtons);
     if (Number(clickedButton) == winnerButton) {
         document.getElementById("output").innerHTML = "Congratulations. You guessed the button!"
     } else {
